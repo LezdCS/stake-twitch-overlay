23e35bf9686370b533b16a342647bb980cf1ac6c
Lezd <lilinouxgaming@gmail.com>
2022-08-09 01:52:19 +0200

    Fix cookie expires date

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -10,14 +10,31 @@ type SettingsProps = {
 
 const Settings: React.FC<SettingsProps> = ({token, setToken, wagerGoal, setWagerGoal}) => {
 
+    function setCookie(cName:string, cValue:string, expDays:number) {
+        let date = new Date();
+        date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
+    }
+
+    const handleChangWagerGoal = (e:any) => {
+        setCookie("WAGER_GOAL", e.target.value, 365)
+        setWagerGoal(e.target.value)
+    }
+
+    const handleChangeToken = (e:any) => {
+        setCookie("TOKEN", e.target.value, 365)
+        setToken(e.target.value)
+    }
+
     return (
         <div className='settings'> 
             <label>API Token:
-                <input value={token} onChange={e => {document.cookie = `TOKEN=${(e.target.value)}`; setToken(e.target.value)}} type="password"></input>
+                <input value={token} onChange={handleChangeToken} type="password"></input>
             </label>
 
             <label>Wager goal:
-                <input type="text" value={wagerGoal} onChange={e => {document.cookie = `WAGER_GOAL=${(e.target.value)}`; setWagerGoal(e.target.value)}}></input>
+                <input type="text" value={wagerGoal} onChange={handleChangWagerGoal}></input>
             </label>
         </div>
     );
